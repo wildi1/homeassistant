f0b630a7cb438836c09f5d2f5cbe33c0cce34551
wildi1 <stefan.wild@sewisoft.de>
2019-03-17 20:03:09 +0100

    [IMP] Refactor mqtt echo to mqtt stater

--- mqtt_stater/mqtt_echo.py
+++ mqtt_stater/mqtt_echo.py
@@ -6,9 +6,7 @@ with open('options.json') as fp:
     options = json.load(fp)
 
 echo_topics = options['echo_topics']
-fix_values = options['fix_values']
 
-state_values = {}
 dirty = {}
 
 
@@ -23,11 +21,7 @@ def on_message(client, userdata, msg):
     for topic in echo_topics:
         if msg.topic == topic['command']:
             new_vals = json.loads(msg.payload)
-            status_topic = topic['state']
-            if status_topic not in state_values:
-                state_values[status_topic] = {}
-
-            for fix_val in fix_values:
+            for fix_val in topic['fix_values']:
                 if fix_val['key_in'] in new_vals.keys():
                     val = fix_val['val']
                     try:
@@ -35,19 +29,23 @@ def on_message(client, userdata, msg):
                     except ValueError as e:
                         if val.isdigit():
                             val = float(val)
-                    new_vals[fix_val['key_out']] = val
+                    dirty[topic['state']] = {
+                        'state_count': 0,
+                        'state_fire': topic['state_fire'],
+                        'val': val
+                    }
+                    log("Update: " + str(msg.topic) + " " + str(msg.payload))
+                    return
 
-            state_values[status_topic].update(new_vals)
-            dirty[status_topic] = 1
-
-            log("Update: " + str(msg.topic) + " " + str(msg.payload))
-            return
-
-    if dirty[msg.topic] and msg.topic in state_values:
-        data = json.dumps(state_values[msg.topic])
-        client.publish(msg.topic, data)
-        dirty[msg.topic] = 0
-        log("Publish: " + str(msg.topic) + " " + str(data))
+        elif msg.topic == topic['state'] and msg.topic in dirty:
+            dirty[msg.topic]['state_count'] += 1
+            if dirty[msg.topic]['state_count'] == dirty[msg.topic]['state_fire']:
+                state_vals = json.loads(msg.payload)
+                entry = dirty.pop(msg.topic)
+                state_vals.update(entry['val'])
+                data = json.dumps(state_vals)
+                log("Publish: " + str(msg.topic) + " " + str(data))
+                client.publish(msg.topic, data)
 
 
 def log(msg):
